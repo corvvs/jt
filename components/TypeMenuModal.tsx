b5665ff127440a33143ab41fc3c8a95bb8367bb5
河田鷹介 <benefium@gmail.com>
2023-05-25 20:49:59 +0900

    一旦これで

---
+++ components/TypeMenuModal.tsx
@@ -0,0 +1,7 @@
+export const TypeMenuModal = () => {
+  return <div
+    className="absolute z-[9999] bg-black/30"
+  >
+
+  </div>
+}
--- components/json/JsonStructure.tsx
+++ components/json/JsonStructure.tsx
@@ -1,10 +1,11 @@
 import { JSONArrayValueStruct, JSONBooleanValueStruct, JSONNullValueStruct, JSONNumberValueStruct, JSONObjectValueStruct, JSONStringValueStruct, JSONValueStruct, jsonItemCount } from "@/libs/json"
-import React from "react";
+import React, { useRef, useState } from "react";
 import { MdDataArray, MdDataObject } from 'react-icons/md';
 import { BsToggleOff, BsToggleOn } from 'react-icons/bs';
 import { TiSortAlphabetically, TiSortNumerically } from 'react-icons/ti';
 import { InlineIcon } from "../lv1/InlineIcon";
 import _ from "lodash";
+import { Popover } from "@headlessui/react";
 
 
 const IconString = TiSortAlphabetically;
@@ -38,12 +39,31 @@ const ActualIconForType = (props: {
   }
 };
 
-const IconForType = (props: {
+const JsonTypeButton = (props: {
   json: JSONValueStruct;
 }) => {
+  const [isOpen, setIsOpen] = useState(false);
+
   const Icon = ActualIconForType(props);
   if (!Icon) { return null; }
-  return <InlineIcon className="text-xl" i={<Icon className="text-xl" />} />
+
+  return <Popover className="relative">
+    {({ open }) => (<>
+      <Popover.Button
+        className="json-type-button outline-none rounded-full hover:border-[1px]"
+      >
+        <InlineIcon className="text-xl" i={<Icon className="text-xl" />} />
+      </Popover.Button>
+
+      <Popover.Panel className="absolute z-[5000]">
+        <div className="flex flex-col p-2 rounded-lg bg-black/50">
+          <div>
+            <h3 className="whitespace-nowrap">Type: {props.json.typename}</h3>
+          </div>
+        </div>
+      </Popover.Panel>
+    </>)}
+  </Popover>
 }
 
 type JsoCommonStructureProps = {
@@ -63,7 +83,7 @@ const JsonStringStructure = (props: JsoCommonStructureProps & {
   js: JSONStringValueStruct;
 }) => {
   return (<div className="json-structure item-value json-string-structure flex items-center">
-    <p className="">
+    <p className="max-w-[48em] text-ellipsis whitespace-nowrap overflow-hidden">
       &quot;{props.js.value}&quot;
     </p>
   </div>);
@@ -73,7 +93,7 @@ const JsonNumberStructure = (props: JsoCommonStructureProps & {
   js: JSONNumberValueStruct;
 }) => {
   return (<div className="json-structure item-value json-number-structure flex items-center">
-    <p className="">
+    <p className="max-w-[48em] text-ellipsis whitespace-nowrap overflow-hidden">
       {props.js.value}
     </p>
   </div>);
@@ -95,7 +115,7 @@ const JsonObjectStructure = (props: JsoCommonStructureProps & {
   js: JSONObjectValueStruct;
 }) => {
   const depth = ((props.depth ?? 0) + 1) % 5;
-  return (<div className="json-structure json-object-structure grid grid-cols-[max-content_1fr] justify-stretch">
+  return (<div className="json-structure json-object-structure relative grid grid-cols-[max-content_1fr] justify-stretch">
     {
       props.js.subtree.length === 0 ? null : <>
         <p className="schema-key">
@@ -110,20 +130,20 @@ const JsonObjectStructure = (props: JsoCommonStructureProps & {
       const key = `${props.jkey ? props.jkey + "." : ""}${k}`;
       const is_structural = value.typename === "object" || value.typename === "array";
       const keyClassName = is_structural
-        ? `item-key flex items-start depth-${depth}`
-        : `item-key flex items-center depth-${depth}`;
+        ? `item-key relative flex items-start depth-${depth}`
+        : `item-key relative flex items-center depth-${depth}`;
       const itemCount = jsonItemCount(value);
       return (
         <React.Fragment key={key}>
           <div className={keyClassName}>
             <div className="sticky top-0 flex items-center p-1 gap-1">
-              {is_structural ? null : <IconForType json={value} /> }
+              {is_structural ? null : <JsonTypeButton json={value} /> }
               <p className="key max-w-[16em] text-ellipsis whitespace-nowrap overflow-hidden">{k}</p>
-              {is_structural ? <IconForType json={value} /> : null }
+              {is_structural ? <JsonTypeButton json={value} /> : null }
               { _.isFinite(itemCount) ? <p>({itemCount})</p> : null }
             </div>
           </div>
-          <div className="item-value flex items-center">
+          <div className="item-value relative flex items-center">
             <JsonStructure jkey={key} depth={(props.depth ?? 0) + 1} js={value} />
           </div>
         </React.Fragment>
@@ -136,7 +156,7 @@ const JsonArrayStructure = (props: JsoCommonStructureProps & {
   js: JSONArrayValueStruct;
 }) => {
   const depth = ((props.depth ?? 0) + 1) % 5;
-  return (<div className="json-structure json-array-structure grid grid-cols-[max-content_1fr] justify-stretch">
+  return (<div className="json-structure json-array-structure relative grid grid-cols-[max-content_1fr] justify-stretch">
     {
       props.js.subarray.length === 0 ? null : <>
         <p className="schema-index">
@@ -151,21 +171,21 @@ const JsonArrayStructure = (props: JsoCommonStructureProps & {
       const key = `${props.jkey ? props.jkey + "." : ""}${i}`;
       const is_structural = v.typename === "object" || v.typename === "array";
       const keyClassName = is_structural
-        ? `item-index flex items-start depth-${depth}`
-        : `item-index flex items-center depth-${depth}`;
+        ? `item-index relative flex items-start depth-${depth}`
+        : `item-index relative flex items-center depth-${depth}`;
       const itemCount = jsonItemCount(v);
       return (<React.Fragment key={key}>
 
         <div className={keyClassName}>
           <div className="sticky top-0 flex items-center p-1 gap-1">
-            {is_structural ? null: <IconForType json={v} /> }
+            {is_structural ? null : <JsonTypeButton json={v} /> }
             <p className="index">#{i}</p>
-            {is_structural ? <IconForType json={v} /> : null }
+            {is_structural ? <JsonTypeButton json={v} /> : null }
             { _.isFinite(itemCount) ? <p>({itemCount})</p> : null }
           </div>
         </div>
 
-        <div className="item-value flex items-center">
+        <div className="item-value relative flex items-center">
           <JsonStructure jkey={key} depth={(props.depth ?? 0) + 1} js={v} />
         </div>
 
