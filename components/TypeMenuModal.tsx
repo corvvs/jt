956f1e130b03a4282ec109f4d6b8b57dde48d7e8
河田鷹介 <benefium@gmail.com>
2023-06-16 21:50:48 +0900

    JSON編集時, 画面切り替えではなくモーダルを出す

--- components/TypeMenuModal.tsx
+++
@@ -1,7 +0,0 @@
-export const TypeMenuModal = () => {
-  return <div
-    className="absolute z-[9999] bg-black/30"
-  >
-
-  </div>
-}
---
+++ components/json/EditJsonCard.tsx
@@ -0,0 +1,41 @@
+import { useJSON } from "@/states";
+import { VscJson } from 'react-icons/vsc';
+import { InlineIcon } from "../lv1/InlineIcon";
+import { JetButton } from "../lv1/JetButton";
+
+export const EditJsonCard = (props: {
+  closeModal: () => void;
+}) => {
+  const { rawText, setRawtext, setBaseText, flatJsons }  = useJSON();
+  return (
+    <div
+      className='json-text w-[36em] flex flex-col font-mono text-sm border-2 rounded-lg'
+    >
+      <div
+        className="shrink-0 grow-0 flex flex-row gap-2 p-2 text-md"
+      >
+        <div>
+          <JetButton
+            onClick={() => {
+              setBaseText(rawText)
+              props.closeModal()
+            }}
+          >
+            <InlineIcon i={<VscJson />} />
+            変換
+          </JetButton>
+        </div>
+      </div>
+      <div
+        className="shrink grow relative"
+      >
+        <textarea
+          className="w-full h-[24em] outline-none p-2 json-text-textarea"
+          style={{ resize: "none" }}
+          value={rawText}
+          onChange={(e) => setRawtext(e.target.value)}
+        />
+      </div>
+    </div>
+  )
+};
