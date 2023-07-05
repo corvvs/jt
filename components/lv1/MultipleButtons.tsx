351dcecb64f56eb0bd67ffccb184a59026e9caf3
河田鷹介 <benefium@gmail.com>
2023-07-06 01:32:48 +0900

    フック化

--- components/lv1/MultipleButtons.tsx
+++ components/lv1/MultipleButtons.tsx
@@ -1,3 +1,4 @@
+import { useTransientBackdrop } from "@/features/TransientBackdrop";
 import { SyntheticEvent, useRef, useState } from "react";
 
 type MultipleButtonItem<T> = {
@@ -18,34 +19,13 @@ export function MultipleButtons<T extends string>({
   currentKey,
   onClick,
 }: MultipleButtonProps<T>) {
-  const hoveredRef = useRef<HTMLElement | null>(null);
-  const [hoverState, setHoverState] = useState<{
-    key: any,
-    left: number;
-    width: number;
-    visible: boolean;
-    animation: "in" | "out" | "move"
-  }>({
-    key: null,
-    left: 0,
-    width: 0,
-    visible: false,
-    animation: "in",
-  });
+  const {
+    handleMouseEnter,
+    handleMouseLeave,
+    backdrop,
+  } = useTransientBackdrop();
 
-  const handleMouseEnter = (key: any, e: SyntheticEvent<HTMLElement>) => {
-    const animation = hoveredRef.current ? "move" : "in";
-    hoveredRef.current = e.currentTarget as HTMLElement;
-    const width = hoveredRef.current.offsetWidth;
-    const left = hoveredRef.current.offsetLeft
-    setHoverState({ key, left, width, animation, visible: true })
-  };
-  const handleMouseLeave = () => {
-    hoveredRef.current = null;  // マウスが要素から離れたときはnullを設定
-    setHoverState((prev) => {
-      return { ...prev, key: null, animation: "out", visible: false };
-    })
-  };
+
 
   const buttons = items.map(item => {
     const isActive = currentKey === item.key;
@@ -54,30 +34,17 @@ export function MultipleButtons<T extends string>({
       className={`multiple-buttons-button ${isActive ? "active" : ""}`}
       title={item.hint}
       onClick={() => onClick(item)}
-      onMouseEnter={e => handleMouseEnter(item.key, e)}
+      onMouseEnter={handleMouseEnter}
     >
       {item.content ? item.content : item.title || null}
     </button>
   });
 
-  console.log("hoverState", hoverState);
   return <div
     className="multiple-buttons relative"
-    onMouseLeave={e => handleMouseLeave()}
+    onMouseLeave={handleMouseLeave}
   >
-    <div
-      className="transient-backdrop"
-      style={{
-        width: hoverState.width,
-        transform: `translateX(${hoverState.left}px)`,
-        opacity: hoverState.visible ? 1 : 0,
-        ...(
-          hoverState.animation === "move"
-            ? { transitionDuration: "128ms" }
-            : { transition: "opacity 128ms" }
-        ),
-      }}
-    />
+    { backdrop }
     { buttons }
   </div>;
 }
