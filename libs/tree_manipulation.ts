bc4c769467a6016cf4a237cbe45682a8f282791c
河田鷹介 <benefium@gmail.com>
2024-02-11 06:11:09 +0900

    add: Mapキーソートボタン

---
+++ libs/tree_manipulation.ts
@@ -0,0 +1,30 @@
+import _ from "lodash";
+
+/**
+ * 受け取ったJSON文字列のうち, Mapについてのみキーでソートして返す
+ * (jq --sort-keys と思ってよい)
+ */
+export function sortKeysJson(jsonText: string) {
+  const parsed = JSON.parse(jsonText);
+  const stringified = recursiveRestringify(parsed);
+  return stringified;
+}
+
+function recursiveRestringify(item: any): string {
+  if (typeof item !== "object" || _.isNull(item)) {
+    return JSON.stringify(item);
+  }
+
+  if (_.isArray(item)) {
+    return `[${item.map((value) => recursiveRestringify(value)).join(", ")}]`;
+  }
+
+  const keys = _.sortBy(Object.keys(item), key => key);
+
+  const stringified = keys.map((key) => {
+    const value = item[key]!;
+    const stringified = recursiveRestringify(value);
+    return `\"${key}\": ${stringified}`;
+  }).join(", ");
+  return `{${stringified}}`;
+}
